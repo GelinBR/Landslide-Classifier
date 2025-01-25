5953e133d798d457d88c012ac72d7d82ae2d0fb0
Yuri Gelli <yuri.gelli@ufl.edu>
2025-01-25 18:12:35 -0500

    Title properly formatted

--- two-boxes-app/src/App.js
+++ two-boxes-app/src/App.js
@@ -1,10 +1,35 @@
+import React from 'react';
 import './App.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 function App() {
   return (
     <div className="App">
       <h1 className="title">Welcome to the Two Boxes App</h1>
-      <div className="box box1">Box 1</div>
+      
+      <div className="box box1">
+        <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '100%', width: '100%' }}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A sample marker.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
       <div className="box box2">Box 2</div>
     </div>
   );

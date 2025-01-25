8f1cbe6c0d730d8de70d87f28453cdedd575ced4
Yuri Gelli <yuri.gelli@ufl.edu>
2025-01-25 17:51:22 -0500

    Added title slot

--- two-boxes-app/src/App.js
+++ two-boxes-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1 className="title">Welcome to the Two Boxes App</h1>
       <div className="box box1">Box 1</div>
       <div className="box box2">Box 2</div>
     </div>

ccb100b49c102c1ad5b47f51e1055b4078eb2b0f
Yuri Gelli <yuri.gelli@ufl.edu>
2025-01-25 22:44:59 -0500

    GIS loaded but not yet functioning

--- two-boxes-app/src/App.js
+++ two-boxes-app/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+import * as EL from 'esri-leaflet';
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -12,11 +13,29 @@ L.Icon.Default.mergeOptions({
     shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
 
+function FeatureLayer() {
+  const map = useMap();
+
+  React.useEffect(() => {
+    const featureLayer = EL.featureLayer({
+      url: 'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/LA_County_Boundaries/FeatureServer/0'
+    });
+
+    featureLayer.addTo(map);
+
+    return () => {
+      map.removeLayer(featureLayer);
+    };
+  }, [map]);
+
+  return null;
+}
+
 function App() {
   return (
     <div className="App">
       <div className="title-bar">
-        <h1 className="title">Welcome to the Landslide Clasifier</h1>
+        <h1 className="title">Welcome to the Landslide Classifier</h1>
       </div>
       <div className="boxes-container">
         <div className="box box1">
@@ -25,6 +44,7 @@ function App() {
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
+            <FeatureLayer />
             <Marker position={[51.505, -0.09]}>
               <Popup>
                 A sample marker.
